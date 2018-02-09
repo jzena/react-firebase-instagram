a2be3036ac939557dd306c65e31aa17040f3c230
jose.zena <jose.zena@globant.com>
2018-02-09 16:47:41 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import firebase from 'firebase';
+
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDxMApcCbLHgOPcQ6IA9F0NsfLuhZuwHRc",
+    authDomain: "pseudogram-92f4f.firebaseapp.com",
+    databaseURL: "https://pseudogram-92f4f.firebaseio.com",
+    projectId: "pseudogram-92f4f",
+    storageBucket: "pseudogram-92f4f.appspot.com",
+    messagingSenderId: "473672233600"
+});
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
